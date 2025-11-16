79b0032cd0c696297ae146f184bb5d750246e626
isaac <isaacunhaid@gmail.com>
2025-11-16 18:08:33 -0300

    feat: adicionando as imagens no banco do sudoku

--- packages/api/src/api/routes/__init__.py
+++ packages/api/src/api/routes/__init__.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, APIRouter
-from api.routes import sudoku_route
+from api.routes import sudoku_route, reasoner_route
 
 def register_routes(app: FastAPI) -> None:
     v1_router = APIRouter(prefix="/v1")
     v1_router.include_router(sudoku_route.router, prefix="/sudoku", tags=["Sudoku"])
+    v1_router.include_router(reasoner_route.router, prefix="/reasoner", tags=["Reasoner"])
     app.include_router(v1_router)
