974a22454ce6b946b0195e5a3cf94ffeb3096561
nayetdet <jpmoreiracampos21@gmail.com>
2025-11-21 05:50:10 -0300

    feat: adicionado endpoint que possibilita o download de todas as imagens zipadas

--- packages/core/src/core/enums/sudoku_simplified_candidate_type.py
+++ packages/core/src/core/enums/sudoku_simplified_candidate_type.py
@@ -14,3 +14,13 @@ class SudokuSimplifiedCandidateType(Enum):
                 return "Hidden Singles (camada 0)"
             case SudokuSimplifiedCandidateType.FIRST_LAYER_CONSENSUS:
                 return "Consensus (1ª camada)"
+
+    @property
+    def simplified_display_name(self) -> str:
+        match self:
+            case SudokuSimplifiedCandidateType.ZEROTH_LAYER_NAKED_SINGLES:
+                return "naked_singles"
+            case SudokuSimplifiedCandidateType.ZEROTH_LAYER_HIDDEN_SINGLES:
+                return "hidden_singles"
+            case SudokuSimplifiedCandidateType.FIRST_LAYER_CONSENSUS:
+                return "consensus"
