6ec1655345efa7d2ea8a0001e1fd3cc7f4495b7d
isaac <isaacunhaid@gmail.com>
2025-11-21 17:39:44 -0300

    feat: adicionando visualização de imagens no banco via streamlit de forma precisa

--- packages/webui/src/webui/main.py
+++ packages/webui/src/webui/main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from webui.components.galleries.sudoku_image_gallery_component import SudokuImageGalleryComponent
+from webui.components.images.sudoku_image_gallery_component import SudokuImageGalleryComponent
 from webui.components.tables.sudoku_inference_analytics_table_component import SudokuInferenceAnalyticsTableComponent
 from webui.components.tables.sudoku_table_component import SudokuTableComponent
 
